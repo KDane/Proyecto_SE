fb46984c4ce20c137616031aa458a63986f4b42a
KDane <karendane95@gmail.com>
2018-12-03 11:58:55 -0600

    nuevo  commit

--- index.js
+++ index.js
@@ -1,10 +1,82 @@
-//Servidor web en nodeJS para publicar archivos estaticos.
-            var express = require("express");
-            var app = express();
+var express = require("express");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var mysql = require("mysql");
+var app = express();
+var credenciales ={
+  user:"root",
+  password:"",
+  database:"bd_proyecto",
+  host:"localhost",
+  port:"3306"  
+};
 
-            //Exponer una carpeta como publica
-            app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-            //Crear y levantar el servidor web.
-            app.listen(3000);
-            console.log("Servidor iniciado");
+app.use(express.static("public"));
+app.use(session({secret:"ASDFE$%#%",resave:true, saveUninitialized:true}));
+
+//Verificar si existe una variable de sesion para poner publica la carpeta public admin
+var publicAdmin = express.static("public-basico");
+var publicCajero = express.static("public-estandar");
+var publicCajero = express.static("public-premium");
+
+app.use(
+    function(req,res,next){
+        if (req.session.Correo){
+            //Significa que el usuario si esta logueado
+            if (req.session.idPlan == 1)
+                publicbasico(req,res,next);
+            else if (req.session.idPlan == 2)
+                publicestandar(req,res,next);
+                else if (req.session.idPlan == 3)
+                publicpremium(req,res,next);
+        }
+        else
+            return next();
+    }
+);
+
+
+
+app.post("/login",function(req, res){
+    var conexion = mysql.createConnection(credenciales);
+    conexion.query(
+        "SELECT idUsuario, Correo, idPlan FROM usuario WHERE Contrasena = sha1(?) and Correo=?",
+        [req.body.Contrasena, req.body.Correo],
+        function(error, data, fields){
+            if (error){
+                res.send(error);
+                res.end();
+            }else{
+                if (data.length==1){
+                    req.session.idUsuario = data[0].idUsuario;
+                    req.session.Correo = data[0].Correo;
+                    req.session.idPlan = data[0].idPlan;
+                }
+                res.send(data);
+                res.end();
+            }
+        }
+    )
+});
+
+app.get("/obtener-session",function(req,res){
+    res.send("Codigo Usuario: " + req.session.idUsuario+
+            ", Correo: " + req.session.Correo +
+            ", Tipo Usuario: " + req.session.idPlan
+    );
+    res.end();
+});
+
+
+//salir
+app.get("/cerrar-session",function(req,res){
+    req.session.destroy();
+    res.send("Sesion eliminada");
+    res.end();
+});
+
+
+app.listen(8111);
---
+++ public/js/controlador.js
@@ -0,0 +1,20 @@
+
+$("#btn-login").click(function(){
+    console.log($("#formulario").serialize());
+    $.ajax({
+        url:"/login",
+        method:"POST",
+        data:$("#formulario").serialize(),
+        dataType:"json",
+        success:function(resp){
+            console.log(resp);
+            if (resp.length==1)
+                window.location.href = "/Home.html";
+            else 
+                alert("Credenciales invalidas");
+        },
+        error:function(error){
+            console.error(error);
+        }
+    });
+});
